effaccaa3c811d9be1f21bec614cf419d1dc4951
Adi Cohen <cohennadi@gmail.com>
2021-11-10 14:59:52 +0000

    skeletal ptree module

--- ptree_loadable_module/ptree_loadable_module.c
+++ ptree_loadable_module/ptree_loadable_module.c
@@ -1,10 +1,36 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/init.h>
+#include <linux/prinfo.h>
+#include <linux/errno.h>
+#include <linux/string.h>
 
 MODULE_DESCRIPTION ("Ptree loadable module");
 MODULE_LICENSE ("GPL");
 MODULE_INFO(intree, "Y");
+
+int getptree(struct prinfo *buf, int *nr, int pid) 
+{
+	int i =0;
+	
+	if (buf == NULL || nr == NULL || *nr < 1)
+	{
+		return EINVAL;
+	}
+
+	// Do dummy layout
+	for (i = 0; i < *nr; ++i) 
+	{
+		buf[i].parent_pid = i;
+	        buf[i].pid = i + 1;
+		buf[i].state = 0;
+		buf[i].uid = 0;
+		strcpy(buf[i].comm, "dummy") ;
+		buf[i].level = i;	
+	}
+
+	return 0;
+}
 
 static int ptree_module_init (void)
 {
